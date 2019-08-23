8cb2abe419517d3edd1442e184950b9d49242c04
VolkovnytskyiKostiantyn <volkovnytskyi.kostiantyn@devico.io>
2019-08-23 17:47:08 +0300

    added jwt verification

--- modules/verifyToken.js
+++ modules/verifyToken.js
@@ -1,11 +1,17 @@
 export default function verifyToken(req, res, next) {
   const bearerHeader = req.headers.authorization
+  console.log(req.headers)
+  // console.log(`bh ${bearerHeader}`)
   if (typeof bearerHeader !== 'undefined') {
     const bearer = bearerHeader.split(' ')
+    // console.log(`b ${bearer}`)
     const bearerToken = bearer[1]
-    req.token = bearerToken
+    // console.log(`bt ${bearerToken}`)
+    req.usersToken = bearerToken
+    console.log(`VT req.token ${req.usersToken}`)
+    next()
   } else {
-    res.send('Smthng went wrong')
+    console.log('403 from VT')
+    res.sendStatus(403)
   }
-  next()
 }
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ import verifyToken from './modules/verifyToken'
 
 const app = express()
 
-let { todo, idToDelete, idToUpdate, updKeyValue, newUser }
+let todo; let idToDelete; let idToUpdate; let updKeyValue; let newUser; let token;
 
 app.use(express.static(`${__dirname}/public`))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*'])
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   res.append('Access-Control-Allow-Headers', 'Content-Type')
+  res.append('Access-Control-Allow-Headers', 'Authorization')
   next()
 })
 
@@ -42,50 +43,93 @@ MongoClient.connect(url, mongoOptions, (err, client) => {
   })
 })
 
-app.post('/signUp', (req, res) => {
+app.post('/signUp', async (req, res) => {
   const {
     login,
     password,
   } = req.body
-  bcrypt.hash(password, 10)
-    .then((hashedPassword) => {
-      jwt.sign({ login }, 'salt', async (err, token) => { //  { expiresIn: '30s' },
-        newUser = { login, hashedPassword, token }
-        const lookingUser = await checkIfUsersExists(connectedClient, login).catch((err) => {
-          res.send('User already exists!')
-        })
-        const isUserExists = !!lookingUser
-        console.log(isUserExists)
-        if (isUserExists) {
-          //checkin for token validity
-          jwt.verify(token, 'salt', async (err, authData) => {
-            console.log('1')
-            if (err) {
-              console.log('2')
-              res.send(err)
-            } else {
-              //comparing entered pass with hashed copy from DB
-              bcrypt.compare(password, lookingUser.hashedPassword).then((isEqual) => {
-                if (isEqual) {
-                  res.send(token)
-                } else {
-                  res.send('Wrong password!')
-                }
-              })
-            }
-          })
-        } else {
+  await jwt.sign({ login }, 'salt', (err, newToken) => {
+    if (err) {
+      res.send('token creating error')
+    }
+    token = newToken
+  })
+  console.log(`c token ${token}`)
+  const lookingUser = await checkIfUsersExists(connectedClient, login).catch(() => {
+    res.send('User already exists!')
+  })
+  const isUserExists = !!lookingUser
+
+  if (isUserExists) {
+    await bcrypt.compare(password, lookingUser.hashedPassword).then( async (isEqual) => {
+      if (isEqual) {
+        await connectedClient.db('todos').collection('users').updateOne({ login }, { $set:{ token } }, { upsert: true })
+        console.log(token)
+        console.log('sendin token 1')
+        res.send(token)
+      } else {
+        res.sendStatus(403)
+      }
+    })
+  } else {
           // if user doesnt exist - creating new
-          console.log('3')
           try {
+            const hashedPassword = await bcrypt.hash(password, 10)
+            newUser = { login, hashedPassword, token }
+            console.log('3')
             await connectedClient.db('todos').collection('users').insertOne(newUser)
+            console.log('sendin token 2')
+            console.log(token)
             res.send(token)
           } catch (error) {
-            console.log(error)
+            res.send(error)
           }
-        }
-      })
-    })
+  }
+
+
+
+
+
+
+
+  // bcrypt.hash(password, 10)
+  //   .then((hashedPassword) => {
+  //     jwt.sign({ login }, 'salt', async (err, token) => { //  { expiresIn: '30s' },
+  //       newUser = { login, hashedPassword, token }
+  //       const lookingUser = await checkIfUsersExists(connectedClient, login).catch(() => {
+  //         res.send('User already exists!')
+  //       })
+  //       const isUserExists = !!lookingUser
+  //       console.log(isUserExists)
+  //       if (isUserExists) {
+  //         //checkin for token validity
+  //         console.log(req.usersToken)
+  //         jwt.verify(token, 'salt', async (err, authData) => {
+  //           console.log(req.token)
+  //           console.log('1')
+  //           if (err) {
+  //             console.log('2')
+  //             console.log(err)
+  //             res.send(err)
+  //           } else {
+  //             //comparing entered pass with hashed copy from DB
+  //             await bcrypt.compare(password, lookingUser.hashedPassword).then((isEqual) => {
+  //               if (isEqual) {
+  //                 connectedClient.db('todos').collection('users').updateOne({ login }, { $set:{ token } }, { upsert: true })
+  //                 console.log('sendin token')
+  //                 console.log(token)
+  //                 res.send(token)
+  //               } else {
+  //                 res.send('Wrong password!')
+  //               }
+  //             })
+  //           }
+  //         })
+  //       } else {
+
+  //       }
+  //     })
+  //   })
 })
 
 app.post('/', verifyToken, (req, res) => {
@@ -100,11 +144,16 @@ app.post('/', verifyToken, (req, res) => {
 })
 
 app.get('/', verifyToken, (req, res, next) => {
-  callGetTodos(connectedClient)
-    .then((result) => {
-      res.json(result)
-    })
-    .catch((err) => console.log(err))
+  jwt.verify(req.token, 'salt', (err, authData) => {
+      console.log('gettin todos ')
+      callGetTodos(connectedClient)
+      .then((result) =>res.json(result))
+      .catch((err) => {
+        console.log('403 from get')
+        console.log(err)
+        res.sendStatus(403)
+      })
+  })
 })
 
 app.delete('/', verifyToken, (req, res) => {
